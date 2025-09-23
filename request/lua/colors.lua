d03a940e2f5677e778fa4224dca1e565c53badb9
Taylor Bartlett <taybart@gmail.com>
2025-09-23 17:15:02 -0600

    feat: add hyperlinks in colors.lua

--- request/lua/colors.lua
+++ request/lua/colors.lua
@@ -5,7 +5,7 @@ local ceItalic = '\27[3;'
 local ceUnderlined = '\27[4;'
 local ceBlinking = '\27[5;'
 
-return {
+local colors = {
   -- Normal Colors
   gray = ce .. '37m',
   purple = ce .. '35m',
@@ -16,6 +16,8 @@ return {
 
   -- Bold Colors
   bold = {
+    -- e's are non colored escapes (used in colors.f)
+    e = '\27[1m',
     gray = ceBold .. '37m',
     purple = ceBold .. '35m',
     blue = ceBold .. '34m',
@@ -26,6 +28,7 @@ return {
 
   -- Italic Colors
   italic = {
+    e = '\27[3m',
     gray = ceItalic .. '37m',
     purple = ceItalic .. '35m',
     blue = ceItalic .. '34m',
@@ -36,6 +39,7 @@ return {
 
   -- Underlined Colors
   underlined = {
+    e = '\27[4m',
     gray = ceUnderlined .. '37m',
     purple = ceUnderlined .. '35m',
     blue = ceUnderlined .. '34m',
@@ -46,6 +50,7 @@ return {
 
   -- Blinking Colors
   blinking = {
+    e = '\27[5m',
     gray = ceBlinking .. '37m',
     purple = ceBlinking .. '35m',
     blue = ceBlinking .. '34m',
@@ -57,3 +62,17 @@ return {
   -- Return to default,
   reset = ce .. '0m',
 }
+
+function colors.f(co, text)
+  return co .. text .. colors.reset
+end
+
+function colors.hyperlink(url, text, color)
+  local ret = '\27]8;;' .. url .. '\27\\' .. text .. '\27]8;;\27\\'
+  if color then
+    return color .. ret .. colors.reset
+  end
+  return ret
+end
+
+return colors
