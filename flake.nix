5b323d1c2751d3a02eb8db95a82c420594697cdb
jacbart <jacbart@gmail.com>
2025-10-05 17:50:10 -0700

    feat(flake): simple flake for golang, added in a default and shell too for other tooling if wanted

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "rest flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    {
+      nixpkgs,
+      self,
+      ...
+    }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+      version = "0.5.0";
+    in
+    {
+      # nix run/build
+      packages = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.callPackage ./default.nix { inherit self pkgs version; };
+        }
+      );
+      # nix develop
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.callPackage ./shell.nix { };
+        }
+      );
+      # nix fmt
+      formatter = nixpkgs.lib.genAttrs allSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+    };
+}
